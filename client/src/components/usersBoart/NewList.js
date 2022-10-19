5a65d4437c82fac6abdd4b693a6271843becbb81
Leandro Espinasse <99082701+LEANDROESPINASSE@users.noreply.github.com>
2022-10-19 01:28:21 -0300

    mylist

--- client/src/components/usersBoart/NewList.js
+++ client/src/components/usersBoart/NewList.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import "./NewList.css"
 
 const NewList = ({onCreate}) => {
     const [isEdit, setIsEdit] = useState(false);
@@ -16,24 +17,28 @@ const NewList = ({onCreate}) => {
           setIsEdit(false);
         }
         return (
+
             <form className="NewListForm" onSubmit={handleSubmit}>
               <input
                 type="text"
                 label='new list'
+                placeholder='List name...'
                 className="listInput"
                 onChange={handleChange}
                 value={newValue}
               />
-              <button className="buttonCreate" onClick={handleClickCreate}>
-                Create
+              <button className="buttonListcreate" onClick={handleClickCreate}>
+                Create List
               </button>
             </form>
+
           );}
     function ButtonNewLIst(){
         return(
-        <button className='ButtonNewLIst' onClick={() => setIsEdit(true)}><svg width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+          <div className='ButtonNewLIstContainer'>
+        <button className='ButtonNewLIst' onClick={() => setIsEdit(true)}><svg width="28" height="28" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
-      </svg></button>)
+      </svg></button></div>)
     }
   return (
     <>
